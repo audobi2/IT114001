d9308387d2a5eb9a6e7d527cb0858faf45c9c509
audobii <ato5@njit.edu>
2020-09-17 23:49:48 -0400

    uploading Java Data hw

---
+++ Homework/src/JavaData.java
@@ -0,0 +1,17 @@
+
+public class JavaData {
+	public static void main(String[] args) {
+		// 1. Create a variable that holds a name
+		String name = "Audrey";
+		// 2. Output the name to the console
+		System.out.println(name);
+		// 3. Create a variable that holds an age (as a whole number)
+		int age = 19;
+		// 4. Output the age to the console
+		System.out.println(age);
+		// 5. Create a variable that holds a decimal
+		float d = 17.8f;
+		// 6. Output the variable
+		System.out.println(d);
+	}
+}
